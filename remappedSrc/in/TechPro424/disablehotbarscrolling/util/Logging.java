d33c60b3d6d6d5d8e8fac9ac616c11b5c47b95a3
TechPro424 <smsvra6@gmail.com>
2023-04-10 13:04:10 +0530

    Delete files that are no longer needed

--- remappedSrc/in/TechPro424/disablehotbarscrolling/util/Logging.java
+++
@@ -1,8 +0,0 @@
-package in.TechPro424.disablehotbarscrolling.util;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class Logging {
-    public static final String MOD_ID = "itemblacklist";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-}
